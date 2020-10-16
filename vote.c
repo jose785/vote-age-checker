b1891e4a0661e0704e3753b2960227167156eb2a
jose785 <57173152+jose785@users.noreply.github.com>
2020-10-16 14:36:26 +0530

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int  main () {
+
+int a;
+
+printf("enter your age");
+scanf("%d",&a);
+if(a>=18)
+
+{
+
+printf("your eligible for vote");
+}
+
+else{
+printf("your not eligible vote ");
+
+}
+return 0;}
+
